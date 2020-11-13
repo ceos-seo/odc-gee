96738c99decf9642638e0346c6db5c53d9b4778e
Andrew M. Lubawy <andrew.m.lubawy@ama-inc.com>
2020-11-13 15:40:23 -0500

    Updated documentation

--- README.md
+++ README.md
@@ -6,23 +6,25 @@ This package provides various tools for indexing [Google Earth Engine
 images into an [Open Data Cube
 (ODC)](https://datacube-core.readthedocs.io/en/latest/index.html) index.  It
 takes advantage of the [GEE REST
-API](https://developers.google.com/earth-engine/reference). Therefore it will
-require Google service account credentials and a GEE API key in order to work.
+API](https://developers.google.com/earth-engine/reference).
 
 ## Installation
 The package can be installed using Python setuptools:
 `python setup.py build && python setup.py install`
 
+Alternatively:
+`pip install -e odc_gee`
+
 ## Configuration
 The scripts and python modules in this package use the following environment
 variables:
 
-* `DATACUBE_CONFIG_PATH`: the ODC configuration file.
-* `GOOGLE_APPLICATION_CREDENTIALS`: the service account credentials JSON file
-  (default: ~/.config/odc-gee/credentials.json).
-* `EE_API_KEY`: the API key for the GEE API.
-* `REGIONS_CONFIG`: a JSON file for storing latitude/longitude locations if not
-  performing global indexing (default: ~/.config/odc-gee/regions.json).
+* `DATACUBE_CONFIG_PATH`: Optional; the ODC configuration file.
+* `GOOGLE_APPLICATION_CREDENTIALS`: Optional; the service account credentials
+  JSON file (default: ~/.config/odc-gee/credentials.json).
+* `REGIONS_CONFIG`: Optional; a JSON file for storing latitude/longitude
+  locations if not performing global indexing (default:
+~/.config/odc-gee/regions.json).
 
 Some example config files are provided in the `./opt/config` directory.
 
@@ -39,39 +41,3 @@ procedure](https://datacube-core.readthedocs.io/en/latest/ops/product.html).
 The package also provides Python modules and optional utilities like systemd
 timer and service for automated indexing. Modules can be accessed as such:
 `import odc_gee`.
-
-### Project Structure
-
-	 ├── docs
-	 │   ├── images
-	 │   │   ├── image1.png
-	 │   │   ├── image2.png
-	 │   │   └── image3.png
-	 │   ├── ODC-GEE_Guide.docx
-	 │   ├── ODC-GEE_Guide.pdf
-	 │   └── ODC-GEE_Guide.tex (extra background info on the project)
-	 ├── odc_gee
-	 │   ├── earthengine.py (GEE REST API utilities)
-	 │   ├── indexing.py (indexing modules for creating dataset documents and adding to the database)
-	 │   ├── __init__.py
-	 │   ├── logger.py (a logging wrapper for system logging [currently unused])
-	 │   └── parser.py (parses GEE metadata from REST API)
-	 ├── opt
-	 │   ├── config
-	 │   │   ├── datacube-core
-	 │   │   │   └── datacube.conf (example ODC configuration)
-	 │   │   ├── odc-gee
-	 │   │   │   └── regions.json (example region definitions)
-	 │   │   └── systemd
-	 │   │       └── user
-	 │   │           ├── update_products.service (systemd user service to run update_products)
-	 │   │           └── update_products.timer (systemd user timer to autorun service)
-	 │   └── local
-	 │       └── bin
-	 │           └── update_products (example bash script for systemd service use)
-	 ├── scripts
-	 │   ├── index_gee (uses the odc_gee modules to index for defined products)
-	 │   └── new_product (guides users through creating a new product defintion)
-	 ├── .gitignore
-	 ├── README.md (odc-gee usage and setup)
-	 └── setup.py
--- odc_gee/earthengine.py
+++ odc_gee/earthengine.py
@@ -15,6 +15,13 @@ SCOPES = ['https://www.googleapis.com/auth/earthengine',
 HOME = os.getenv('HOME')
 
 class Datacube(datacube.Datacube):
+    ''' Extended Datacube object for use with Google Earth Engine.
+
+    Attributes:
+        credentials: The Earth Engine credentials being used for the API session.
+        request: The Request object used in the session.
+        ee: A reference to the ee (earthengine-api) module.
+    '''
     def __init__(self, *args,
                  credentials=os.getenv('GOOGLE_APPLICATION_CREDENTIALS',
                                        f'{HOME}/.config/odc-gee/credentials.json'),
@@ -32,10 +39,21 @@ class Datacube(datacube.Datacube):
         super().__init__(*args, **kwargs)
 
     def load(self, *args, **kwargs):
+        ''' An overloaded load function from Datacube.
+
+        This load method allows for querying the Earth Engine REST API to search for datasets
+        instead of using the standard database query of datacube.Datacube.
+
+        Args:
+            asset (str): The asset ID of the GEE collection or image being queried. If not included
+                then the load will default to normal Datacube operation.
+
+        Returns: The queried xarray.Dataset.
+        '''
         if kwargs.get('asset'):
-            params, kwargs = self.build_parameters(**kwargs)
+            parameters, kwargs = self.build_parameters(**kwargs)
             try:
-                images = self.get_images(params)
+                images = self.get_images(parameters)
                 if kwargs.get('product') and not isinstance(kwargs.get('product'),
                                                             datacube.model.DatasetType):
                     kwargs.update(product=self.index.products.get_by_name(kwargs['product']))
@@ -60,53 +78,88 @@ class Datacube(datacube.Datacube):
         os.environ.update(EEDA_BEARER=self.credentials.token)
         return True
 
-    def get_images(self, params):
+    def get_images(self, parameters):
+        ''' Gets the images or image from the GEE REST API.
+
+        Args:
+            parameters (dict): The parameters to use for the REST API query.
+
+        Returns: The response from the API.
+        '''
         try:
             request = self.ee.data._get_cloud_api_resource().projects().assets().listImages(
-                **params)
+                **parameters)
             while request is not None:
                 response = self.ee.data._execute_cloud_call(request)
                 request = self.ee.data._cloud_api_resource.projects().assets().listImages_next(
                     request, response)
                 for image in response.get('images', []):
                     yield image
-                if 'pageSize' in params:
+                if 'pageSize' in parameters:
                     break
         except self.ee.EEException as error:
             if error.args[0].find('is not an image collection.') != -1:
-                params = dict(name=params['parent'])
-                request = self.ee.data._get_cloud_api_resource().projects().assets().get(**params)
+                parameters = dict(name=parameters['parent'])
+                request = self.ee.data._get_cloud_api_resource().projects().assets().get(
+                    **parameters)
                 response = self.ee.data._execute_cloud_call(request)
                 yield response
         except Exception as error:
             raise error
 
     def build_parameters(self, **kwargs):
-        params = dict(parent=self.ee.data.convert_asset_id_to_asset_name(kwargs['asset']))
+        ''' Build query parameters for GEE the REST API.
+
+        Args:
+            asset (str): The asset ID of the image or image collection.
+            latitude (tuple/list): Optional; the latitude extents to search.
+            longitude (tuple/list): Optional; the longitude extents to search.
+            time (str/tuple/list): Optional; the time extent to search.
+            query (dict): Optional; extra parameters to add to the query.
+
+        Returns:
+            A tuple of a dict of query parameter and the original kwargs dict
+            with the query key removed if provided.
+        '''
+        asset = kwargs['asset']
+        parameters = dict(parent=self.ee.data.convert_asset_id_to_asset_name(asset))
         if kwargs.get('latitude') and kwargs.get('longitude'):
-            params.update(region=ee.Geometry.Rectangle(coords=(kwargs['longitude'][0],
-                                                               kwargs['latitude'][0],
-                                                               kwargs['longitude'][1],
-                                                               kwargs['latitude'][1])).getInfo())
+            parameters.update(
+                region=ee.Geometry.Rectangle(coords=(kwargs['longitude'][0],
+                                                     kwargs['latitude'][0],
+                                                     kwargs['longitude'][1],
+                                                     kwargs['latitude'][1])).getInfo())
         if kwargs.get('time'):
             if isinstance(kwargs['time'], (list, tuple)):
-                params.update(startTime=numpy.datetime64(kwargs['time'][0])\
+                parameters.update(startTime=numpy.datetime64(kwargs['time'][0])\
                               .item().strftime('%Y-%m-%dT%H:%M:%SZ'))
-                params.update(endTime=numpy.datetime64(kwargs['time'][-1])\
+                parameters.update(endTime=numpy.datetime64(kwargs['time'][-1])\
                               .item().strftime('%Y-%m-%dT%H:%M:%SZ'))
             else:
-                params.update(startTime=numpy.datetime64(kwargs['time'])\
+                parameters.update(startTime=numpy.datetime64(kwargs['time'])\
                               .item().strftime('%Y-%m-%dT%H:%M:%SZ'))
         if kwargs.get('query'):
-            params.update(**kwargs['query'])
+            parameters.update(**kwargs['query'])
             kwargs.pop('query')
-        return params, kwargs
+        return parameters, kwargs
 
-    def generate_product(self, **kwargs):
-        stac_metadata = self.get_stac_metadata(kwargs['asset'])
-        metadata = self.ee.data.getAsset(kwargs['asset'])
+    # TODO: Need to determine how to handle measurements.
+    def generate_product(self, asset=None, name=None,
+                         resolution=None, output_crs=None, **kwargs):
+        ''' Generates an ODC product from GEE asset metadata.
 
-        name = kwargs.get('name', metadata.get('id').split('/')[-1])
+        Args:
+            asset (str): The asset ID of the GEE image or image collection.
+            name (str): Optional; the product name.
+            resolution (tuple): Optional; the desired output resolution of the product.
+            output_crs (str): Optional; the desired CRS of the product.
+
+        Returns: A datacube.model.DatasetType product.
+        '''
+        stac_metadata = self.get_stac_metadata(asset)
+        metadata = self.ee.data.getAsset(asset)
+
+        name = name if name else metadata.get('id').split('/')[-1]
         if kwargs.get('measurements') and not isinstance(kwargs['measurements'], (tuple, list)):
             measurements = kwargs['measurements']
         else:
@@ -122,13 +175,20 @@ class Datacube(datacube.Datacube):
                                                     stac_metadata['properties']\
                                                     .get('eo:instrument')}),
                           measurements=measurements)
-        if kwargs.get('resolution') and kwargs.get('output_crs'):
-            definition.update(storage=dict(crs=kwargs['output_crs'],
-                                           resolution=dict(latitude=kwargs['resolution'][0],
-                                                           longitude=kwargs['resolution'][1])))
+        if resolution and output_crs:
+            definition.update(storage=dict(crs=output_crs,
+                                           resolution=dict(latitude=resolution[0],
+                                                           longitude=resolution[1])))
         return self.index.products.from_doc(definition)
 
     def get_measurements(self, metadata):
+        ''' Gets the measurements of a product from the GEE metadata.
+
+        Args:
+            metadata (dict): The metadata from GEE for the desired product.
+
+        Returns: A generated list of datacube.model.Measurement objects.
+        '''
         try:
             band_types = ee.ImageCollection(metadata['id']).first().bandTypes().getInfo()
         except self.ee.EEException as error:
@@ -163,16 +223,37 @@ class Datacube(datacube.Datacube):
                 yield datacube.model.Measurement(**measurement)
 
     def get_stac_metadata(self, asset):
+        ''' Gets STAC metadata of an asset in the GEE catalog.
+
+        Args:
+            asset (str): The asset ID.
+
+        Returns: A dictionary of the metadata.
+        '''
         url = f'gs://earthengine-stac/catalog/{asset.replace("/", "_")}.json'
         blob = self.ee.Blob(url)
         return self.ee.Dictionary(blob.string().decodeJSON()).getInfo()
 
 def generate_documents(images, product):
+    ''' Generates Datacube dataset documents from GEE image data.
+
+    Args:
+        images (list): A list of image data from the GEE API.
+        product (datacube.model.DatasetType): A product to associate datasets with.
+    Returns: A generated list of datacube.model.Dataset objects.
+    '''
     from odc_gee.indexing import make_metadata_doc
     for image in images:
         yield make_metadata_doc(image, product)
 
 def get_type(band_type):
+    ''' Gets the band unit type from GEE metadata.
+
+    Args:
+        band_type (dict): A GEE bandType metadata object.
+
+    Returns: The data type of the band.
+    '''
     types = [numpy.iinfo(numpy.dtype(f'int{2**i}')) for i in range(3, 7)]\
              + [numpy.iinfo(numpy.dtype(f'uint{2**i}')) for i in range(3, 7)]\
              + [numpy.finfo(numpy.dtype(f'float{2**i}')) for i in range(4, 8)]
@@ -180,11 +261,27 @@ def get_type(band_type):
                        and x.max == band_type['max'] else None, types))[0]
 
 def to_snake(string):
+    ''' Cleans and formats strings from GEE metadata into snake case.
+
+    Args:
+        string (str): The string to clean and format.
+
+    Returns: A cleaned string in snake case format.
+    '''
     from re import sub, split
     return sub(r'[, ]+', '_',
                split(r'( \()|[.]', string)[0].replace('/', 'or').replace('&', 'and').lower())
 
-def get_datasets(images, **kwargs):
-    for document in generate_documents(images, kwargs['product']):
-        yield datacube.model.Dataset(kwargs['product'], document,
-                                     uris=f'EEDAI://{kwargs.get("asset")}')
+def get_datasets(asset=None, images=None, product=None):
+    ''' Gets datasets for a Datacube load.
+
+    Args:
+        asset (str): The asset ID of the GEE asset.
+        images (list): A list of image data from the GEE API.
+        product (datacube.model.DatasetType): The product to associate dataset with.
+
+    Returns: A generated list of datacube.model.Dataset objects.
+    '''
+    for document in generate_documents(images, product):
+        yield datacube.model.Dataset(product, document,
+                                     uris=f'EEDAI://{asset}')
