e9bd9918fde427cb80dc5fd58bad675c1ebe6c79
Andrew M. Lubawy <andrew.m.lubawy@ama-inc.com>
2021-08-04 11:29:48 -0400

    Fixed indexing script's test

--- tests/scripts/test_index_gee.py
+++ tests/scripts/test_index_gee.py
@@ -2,21 +2,22 @@ from pathlib import Path
 import subprocess
 import unittest
 
-from datacube import Datacube
-
 from tests.odc_gee.test_indexing import IndexerTestCase
+
+from datacube import Datacube
 
 DATACUBE_CONFIG = f'{Path(__file__).parent.parent.absolute()}/datacube.conf'
 TEST_FILE = f'{Path(__file__).parent.absolute()}/ls8_test.yaml'
 
 class IndexGEETestCase(unittest.TestCase):
     def setUp(self):
+        self.datacube = Datacube(config=DATACUBE_CONFIG)
+
         IndexerTestCase().test_product_generation()
-        indexer = IndexerTestCase().test_init()
-        product = indexer.datacube.index.products.get_by_name('ls8_test')
+        product = self.datacube.index.products.get_by_name('ls8_test')
         if product is None:
             self.skipTest('No product available to index')
-        datasets = indexer.datacube.find_datasets(product='ls8_test')
+        datasets = self.datacube.find_datasets(product='ls8_test')
         if datasets:
             self.skipTest('Indexed datasets already exist in database')
 
@@ -25,12 +26,12 @@ class IndexGEETestCase(unittest.TestCase):
         latitude = (-4.15, -3.90)
         longitude = (39.50, 39.75)
         time = '2020-01'
+
         cmd = ["index_gee", "--product", product, "--latitude", str(latitude),
-               "--longitude", str(longitude), "--time", time, "--config", DATACUBE_CONFIG,
-               "--no_confirm", "-u"]
+               "--longitude", str(longitude), "--time", time,
+               "--config", DATACUBE_CONFIG, "--no_confirm", "-u"]
         subprocess.check_output(cmd)
-        datacube = Datacube(config=DATACUBE_CONFIG)
-        datasets = datacube.find_datasets(product=product)
+        datasets = self.datacube.find_datasets(product=product)
         self.assertGreater(len(datasets), 0,
                            'Expected to find datasets in index')
 
